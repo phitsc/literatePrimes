d2a27699f13cef9614583b1a10ae3a75faec0131
phitsc <github@ph1.ch>
2020-07-17 17:24:30 +0200

    Use ArrayList for primes. Use meaningful variable names.

--- src/main/java/literatePrimes/PrintPrimes.java
+++ src/main/java/literatePrimes/PrintPrimes.java
@@ -1,82 +1,78 @@
 package literatePrimes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintPrimes {
 
-	private static int[] calculatePrimes(int M, int ORDMAX) {
-		int P[] = new int[M + 1];
+	private static List<Integer> calculatePrimes(int numberOfPrimes, int ORDMAX) {
+		var primes = new ArrayList<Integer>(List.of( -1, 2 ));
 
-		int J;
-		int K;
 		boolean JPRIME;
-		int ORD;
-		int SQUARE;
-		int N;
 		int MULT[] = new int[ORDMAX + 1];
 
-		J = 1;
-		K = 1;
-		P[1] = 2;
-		ORD = 2;
-		SQUARE = 9;
+		int J = 1;
+		int ORD = 2;
+		int SQUARE = 9;
 
-		while (K < M) {
+		while (primes.size() <= numberOfPrimes) {
 			do {
 				J = J + 2;
 				if (J == SQUARE) {
 					ORD = ORD + 1;
-					SQUARE = P[ORD] * P[ORD];
+					SQUARE = primes.get(ORD) * primes.get(ORD);
 					MULT[ORD - 1] = J;
 				}
-				N = 2;
+				int N = 2;
 				JPRIME = true;
 				while (N < ORD && JPRIME) {
 					while (MULT[N] < J)
-						MULT[N] = MULT[N] + P[N] + P[N];
+						MULT[N] = MULT[N] + primes.get(N) + primes.get(N);
 					if (MULT[N] == J)
 						JPRIME = false;
 					N = N + 1;
 				}
 			} while (!JPRIME);
-			K = K + 1;
-			P[K] = J;
+			primes.add(J);
 		}
 
-		return P;
+		return primes;
 	}
 
-	private static void printPrimes(int[] P, int M, int RR, int CC) {
-		int PAGENUMBER;
-		int PAGEOFFSET;
-		int ROWOFFSET;
-		int C;
+	private static void printPrimes(List<Integer> primes, int numberOfPrimes, int numberOfRows, int numberOfColumns) {
+		int pageNumber = 1;
+		int pageOffset = 1;
 
-		PAGENUMBER = 1;
-		PAGEOFFSET = 1;
-		while (PAGEOFFSET <= M) {
-			System.out.println("The First " + M +
-                                 " Prime Numbers --- Page "+PAGENUMBER);
+		while (pageOffset <= numberOfPrimes) {
+			System.out.println("The First " + numberOfPrimes +
+                                 " Prime Numbers --- Page " + pageNumber);
 			System.out.println("");
-			for (ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + RR; ROWOFFSET++) {
-				for (C = 0; C < CC; C++)
-					if (ROWOFFSET + C * RR <= M)
-						System.out.format("% 10d",P[ROWOFFSET + C * RR]);
+
+			for (int rowOffset = pageOffset; rowOffset < pageOffset + numberOfRows; rowOffset++) {
+				for (int C = 0; C < numberOfColumns; C++) {
+					if (rowOffset + C * numberOfRows <= numberOfPrimes) {
+						System.out.format("% 10d", primes.get(rowOffset + C * numberOfRows));
+          }
+        }
+
 				System.out.println("");
 			}
+
 			System.out.println("\f");
-			PAGENUMBER = PAGENUMBER + 1;
-			PAGEOFFSET = PAGEOFFSET + RR * CC;
+
+			pageNumber = pageNumber + 1;
+			pageOffset = pageOffset + numberOfRows * numberOfColumns;
 		}
 
 	}
 
 	public static void main(String[] args) {
-		final int M = 1000;
-		final int RR = 50;
-		final int CC = 4;
-		final int WW = 10;
+		final int NumberOfPrimes = 1000;
+		final int NumberOfRows = 50;
+		final int NumberOfColumns = 4;
 		final int ORDMAX = 30;
 
-		int P[] = calculatePrimes(M, ORDMAX);
-		printPrimes(P, M, RR, CC);
+		var primes = calculatePrimes(NumberOfPrimes, ORDMAX);
+		printPrimes(primes, NumberOfPrimes, NumberOfRows, NumberOfColumns);
 	}
 }
